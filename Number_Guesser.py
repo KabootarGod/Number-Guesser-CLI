ac7695378142f9f7b64fb48ab33c7793f4d0248a
AryanSinha <aryansinha1908@gmail.com>
2025-06-19 08:35:08 +0530

    Fixed the highscores problem

--- Number_Guesser.py
+++ Number_Guesser.py
@@ -3,6 +3,12 @@ import json
 
 num = random.randint(1,100)
 
+def prnthigh():
+    with open("highscores.json", 'r') as file:
+        data = json.load(file)
+        print(data)
+
+
 print("""
 Welcome to the Number Guessing Game!
 I'm thinking of a number between 1 and 100.
@@ -12,6 +18,9 @@ Please select the difficulty level:
 1. Easy (7 chances)
 2. Medium (5 chances)
 3. Hard (3 chances)
+
+To view the leaderboard:
+Type 4
       """)
 diff = int(input("Enter your choice: "))
 chances = int()
@@ -23,6 +32,9 @@ match diff:
         chances = 5
     case 3:
         chances = 3
+    case 4:
+        prnthigh()
+        exit()
     case _:
         print("Invalid Input")
         exit()
@@ -41,13 +53,22 @@ while chances != 0:
         name = input("Enter Your name: ")
         with open("highscores.json", 'r+') as file:
             data = json.load(file)
-            if data[name] > ini-chances:
+            '''
+            try data[name] > ini-chances:
                 data.update({name:ini-chances})
                 file.close()
-        print(data)
+            except KeyError:
+                pass'''
+            try:
+                last_score = data[name]
+                if last_score > ini-chances:
+                    data.update({name:ini-chances})
+            except KeyError:
+                data.update({name:ini-chances})
         with open("highscores.json", 'w') as f:
             json.dump(data, f, indent=4)
         break
+
 
 if chances == 0:
     print("Lol! You lost, better luck next time.")
